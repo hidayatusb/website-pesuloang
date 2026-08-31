4866389ccea34dce655205c50082d21d7ac687f1
hidayatusb <muh.hidayatullah@unsulbar.ac.id>
2026-08-31 22:54:17 +0800

    upp

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="description" content="Website resmi Desa Sukamaju, Kec. Cikarang Utara, Kab. Bekasi" />
     <title>@yield('title', 'Desa Sukamaju')</title>
-    <link rel="icon" href="{{ asset('assets/media/app/favicon.ico') }}" />
+    <link rel="icon" href="{{ asset('assets/media/app/favicon-majene.ico') }}" />
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
     <link href="{{ asset('assets/vendors/keenicons/styles.bundle.css') }}" rel="stylesheet" />
     @vite(['resources/css/app.css', 'resources/js/app.js'])
